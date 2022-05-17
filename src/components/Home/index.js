432c1321fcb34b51f1a2be792b57b0f22a133874
NathanM <nathan.milburn@outlook.com>
2022-05-16 20:31:01 -0700

    saving progress

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,14 +1,33 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './index.scss';
+import HeadShot from '../../assets/images/TH-3.PNG'
+import AnimatedLetters from '../AnimatedLetters';
 
 const Home = () => {
-
+    const [letterClass, setLetterClass] = useState('text-animate')
+    const nameArray = ['N', 'a', 't', 'h', 'a', 'n',]
+    const lastNameArray = ['M', 'i', 'l', 'b', 'u', 'r', 'n', ]
 
     return (
         <div className="container home-page">
             <div className="text-zone">
-                <h1>Hi, <br /> I'm
+                <h1>
+                <span className={letterClass}>H</span>
+                <span className={`${letterClass} _12`}>i,</span>
+                <br />
+                <span className={`${letterClass} _13`}>I</span>
+                <span className={`${letterClass} _14`}>'m</span>
+                <AnimatedLetters letterClass={letterClass}
+                strArray={nameArray}
+                idx={15} />
+                <AnimatedLetters letterClass={letterClass}
+                strArray={lastNameArray}
+                idx={22} />
                 </h1>
+                <img src={HeadShot} alt="Professional Headshot" />
+                <h2>Full-stack Developer / YouTuber / Competitive Gamer</h2>
+                <Link to="/contact" className="flat-button">CONTACT ME</Link>
             </div>
 
         </div>
