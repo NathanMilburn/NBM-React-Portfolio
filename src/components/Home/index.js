a5f0b54795aaa8bbce7127c6fe2858e43004ce9b
NathanM <nathan.milburn@outlook.com>
2022-05-16 17:25:41 -0700

    working on home page and saving progress

---
+++ src/components/Home/index.js
@@ -0,0 +1,18 @@
+import { Link } from 'react-router-dom';
+import './index.scss';
+
+const Home = () => {
+
+
+    return (
+        <div className="container home-page">
+            <div className="text-zone">
+                <h1>Hi, <br /> I'm
+                </h1>
+            </div>
+
+        </div>
+    );
+}
+
+export default Home
--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,14 +1,24 @@
 import './index.scss';
 import Navbar from '../Navbar'
+import { Outlet } from 'react-router-dom';
 
 const Layout = () => {
-    return <><Navbar /></>
+    return (
+    <div className="App">
+        <Navbar />
+        <div className='page'>
+            <span className='tags top-tags'>&lt;body&gt;</span>
 
-    // return (
-    // <>
-    // <Navbar />
-    // </>
-    // )
+            <Outlet />
+
+            <span className='tags bottom-tags'>
+            &lt;body&gt;
+            <br />
+            <span className='bottom-tag-html'>&lt;/html&gt;</span>
+            </span>
+        </div>
+    </div>
+    )
 }
 
 export default Layout;
